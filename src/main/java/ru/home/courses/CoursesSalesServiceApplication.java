5c091a46b6e5d43731171079a8bdb65dca2af447
Zest <neo_95@mail.ru>
2025-02-20 12:38:46 +0300

    init

--- src/main/java/ru/home/courses/CoursesSalesServiceApplication.java
+++ src/main/java/ru/home/courses/CoursesSalesServiceApplication.java
@@ -1,15 +1,18 @@
 package ru.home.courses;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class CoursesSalesServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoursesSalesServiceApplication.class, args);
+        log.info("STARTING APP");
     }
 
 }
